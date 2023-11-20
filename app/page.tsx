ef64cc0a2abf1730a36f83e62de8997d0162da97
J_ Keyy <ashnazg1212@gmail.com>
2023-11-20 13:22:06 +0000

    Update page.tsx and Galaxy.jsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import Plane from "../galaxy01/Plane";
+// import Plane from "../galaxy01/Plane";
+
+import Galaxy from "../galaxy01/Galaxy";
 import { useControls } from "leva";
+import { Stars } from "@react-three/drei";
 
 export default function Home() {
   return (
     <Canvas camera={{ position: [0, 6, 14], fov: 42 }}>
-      <color attach="background" args={["#dbecfb"]} />
+      <color attach="background" args={["#000000"]} />
       <OrbitControls />
-      <Plane />
+      {/* <Plane /> */}
+      <Galaxy />
+      {/* <Stars
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      /> */}
     </Canvas>
   );
 }
--- galaxy01/Galaxy.jsx
+++ galaxy01/Galaxy.jsx
@@ -1,16 +1,55 @@
 import { useControls } from "leva";
+import { useMemo } from "react";
+import { AdditiveBlending } from "three";
+import { useControls } from "leva";
 export default function Galaxy() {
-    const parameters={
-        count:100,
+  const parameters = useControls("galaxy", {
+    count: {
+      value: 1000,
+      min: 100,
+      max: 1000,
+      step: 10,
+    },
+    size: {
+      value: 0.02,
+      min: 0,
+      max: 1,
+      step: 0.001,
+    },
+    radius: {
+      value: 2,
+      min: 0,
+      max: 20,
+      step: 0.1,
+    },
+  });
 
+  const positions = useMemo(() => {
+    // console.log("hi");
+    const position = new Float32Array(parameters.count * 3);
+    for (let index = 0; index < parameters.count; index++) {
+      position[index * 3 + 0] = (Math.random() - 0.5) * parameters.radius;
+      position[index * 3 + 1] = 0;
+      position[index * 3 + 2] = 0;
     }
+    return position;
+  }, []);
   return (
-     <points>
-        <bufferGeometry>
-            <bufferAttribute
-            />
-        </bufferGeometry>
-
-  </points>
-  )
+    <points>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={parameters.count}
+          itemSize={3}
+          array={positions}
+        />
+      </bufferGeometry>
+      <pointsMaterial
+        size={parameters.size}
+        sizeAttenuation={true}
+        depthWright={false}
+        blending={AdditiveBlending}
+      />
+    </points>
+  );
 }
