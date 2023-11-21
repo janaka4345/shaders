7593356ae21ba3be233dd40a2a19a9f52e9a23ec
J_ Keyy <ashnazg1212@gmail.com>
2023-11-21 11:01:34 +0000

    Update Sea.jsx

--- sea/Sea.jsx
+++ sea/Sea.jsx
@@ -1,10 +1,11 @@
-import { extend } from "@react-three/fiber";
+import { extend, useFrame } from "@react-three/fiber";
 import { Color, DoubleSide } from "three";
 import { shaderMaterial } from "@react-three/drei";
 import { useControls } from "leva";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Sea() {
+  const seaShaderRef = useRef();
   const parameters = useControls("sea", {
     elevation: {
       value: 0.02,
@@ -12,19 +13,30 @@ export default function Sea() {
       max: 1.0,
       step: 0.001,
     },
+    freq: {
+      value: { x: 4, y: 1.5 },
+      step: 0.1,
+      joystick: "invertY",
+    },
   });
 
   const SeaShaderMaterial = shaderMaterial(
-    { time: 0, uBigWaveElevation: parameters.elevation },
+    {
+      uTime: 0,
+      uBigWaveElevation: parameters.elevation,
+      uBigWaveFreq: parameters.freq,
+    },
     // vertex shader
     /*glsl*/ `
         varying vec2 vUv;
         uniform float uBigWaveElevation;
+        uniform vec2 uBigWaveFreq;
+        uniform float uTime;
         void main() {
           vec4 modelPosition=modelMatrix*vec4(position,1.0);
 
-          float elevation=sin(modelPosition.x*4.0)*
-                            sin(modelPosition.z*1.5)*
+          float elevation=sin(modelPosition.x*uBigWaveFreq.x+uTime)*
+                            sin(modelPosition.z*uBigWaveFreq.y+uTime)*
                             uBigWaveElevation;
           modelPosition.y+=elevation;
   
@@ -51,13 +63,18 @@ export default function Sea() {
 
   // declaratively
   extend({ SeaShaderMaterial });
+  useFrame((status, delta) => {
+    seaShaderRef.current.uTime = status.clock.getElapsedTime();
+  });
 
   return (
     <mesh rotation={[-Math.PI * 0.5, 0, 0]}>
       <planeGeometry args={[2, 2, 128, 128]} />
       <seaShaderMaterial
+        ref={seaShaderRef}
         side={DoubleSide}
         uBigWaveElevation={parameters.elevation}
+        uBigWaveFreq={parameters.freq}
       />
     </mesh>
   );
