f6877e8eae0f5bb4305e8651f37372acff061536
J_ Keyy <ashnazg1212@gmail.com>
2023-11-21 03:53:51 +0000

    Update 3 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,36 @@
 "use client";
+import { useEffect } from "react";
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, extend } from "@react-three/fiber";
 // import Plane from "../galaxy01/Plane";
 
 import Galaxy from "../galaxy01/Galaxy";
 import { useControls } from "leva";
 import { Stars } from "@react-three/drei";
+extend({ Stars });
 
 export default function Home() {
+  let spiralStars = [];
+  useEffect(() => {
+    for (let i = 0; i < 5000; i++) {
+      const angle = i * 0.1;
+      const radius = i * 0.01;
+      const height = i * 0.01;
+
+      const x = radius * Math.cos(angle);
+      const y = 0;
+      const z = radius * Math.sin(angle);
+
+      spiralStars.push([x, y, z]);
+      console.log(spiralStars);
+    }
+  }, []);
   return (
     <Canvas camera={{ position: [0, 6, 14], fov: 42 }}>
       <color attach="background" args={["#000000"]} />
       <OrbitControls />
       {/* <Plane /> */}
       <Galaxy />
-      {/* <Stars
-        radius={100}
-        depth={50}
-        count={5000}
-        factor={4}
-        saturation={0}
-        fade
-        speed={1}
-      /> */}
     </Canvas>
   );
 }
--- galaxy01/Galaxy.jsx
+++ galaxy01/Galaxy.jsx
@@ -2,6 +2,11 @@ import { useControls } from "leva";
 import { useMemo } from "react";
 import { AdditiveBlending } from "three";
 import { useControls } from "leva";
+
+import { extend } from "@react-three/fiber";
+
+import { Stars } from "@react-three/drei";
+extend({ Stars });
 export default function Galaxy() {
   const parameters = useControls("galaxy", {
     count: {
@@ -22,15 +27,45 @@ export default function Galaxy() {
       max: 20,
       step: 0.1,
     },
+    branches: { value: 5, min: 0, max: 20, step: 0.1 },
+    spin: { value: 1 },
+    randomness: { value: 0.2 },
+    randomnessPower: { value: 3 },
+    insideColor: "#ff6030",
+    outsideColor: "#1b3984",
   });
 
   const positions = useMemo(() => {
     // console.log("hi");
     const position = new Float32Array(parameters.count * 3);
-    for (let index = 0; index < parameters.count; index++) {
-      position[index * 3 + 0] = (Math.random() - 0.5) * parameters.radius;
-      position[index * 3 + 1] = 0;
-      position[index * 3 + 2] = 0;
+    for (let i = 0; i < parameters.count; i++) {
+      const i3 = i * 3;
+
+      const radius = Math.random() * parameters.radius;
+
+      const spinAngle = radius * parameters.spin;
+      const branchAngle =
+        ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+      const randomX =
+        Math.pow(Math.random(), parameters.randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        parameters.randomness *
+        radius;
+      const randomY =
+        Math.pow(Math.random(), parameters.randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        parameters.randomness *
+        radius;
+      const randomZ =
+        Math.pow(Math.random(), parameters.randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        parameters.randomness *
+        radius;
+
+      position[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+      position[i3 + 1] = randomY;
+      position[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
     }
     return position;
   }, []);
